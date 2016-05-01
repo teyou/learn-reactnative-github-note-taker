0d3af7467c110f22ec50ee29a73b4197dc7e6219
Rudy <pudding.pearl.tea@gmail.com>
2016-05-01 11:58:25 +0800

    lesson 9

---
+++ App/Components/Badge.js
@@ -0,0 +1,50 @@
+import React,{
+    Text,
+    View,
+    Image,
+    StyleSheet
+} from 'react-native';
+
+var styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#48BBEC',
+        paddingBottom: 10
+    },
+    name : {
+        alignSelf : 'center',
+        fontSize : 21,
+        marginTop : 10,
+        marginBottom : 5,
+        color: 'white',
+    },
+    handle: {
+        alignSelf : 'center',
+        fontSize: 16,
+        color: 'white'
+    },
+    image: {
+        height: 125,
+        width : 125,
+        borderRadius : 65,
+        marginTop : 10,
+        alignSelf : 'center'
+    }
+});
+
+class Badge extends Component{
+    render(){
+        return (
+          <View style={styles.container}>
+            <Image style={styles.iamge} source={{uri: this.porps.userInfo.avatar_url}} />
+            <Text style={styles.name}> {this.props.userInfo.name} </Text>
+            <Text style={styles.handle}> {this.props.userInfo.login} </Text>
+          </View>  
+        );
+    }
+};
+
+Badge.propTypes = {
+    userInfo : React.PropTypes.object.isRequired
+};
+
+module.exports = Badge;
